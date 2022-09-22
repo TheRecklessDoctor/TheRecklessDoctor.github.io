249ad5266454a56e200f5b5a8e9a8eb586a9431f
Dayan Perera <dayan25092002@gmail.com>
2022-09-22 17:57:48 +0800

    minor error fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var vg_1 = "js/visualization.vg.json";
+var vg_1 = "visualization.vg.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance
 // (https://vega.github.io/vega/docs/api/view/) as result.view
